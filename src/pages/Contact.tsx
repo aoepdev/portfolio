c215dca7382d477a4314b0e2c63754cd6e7a14d4
Alfred Emasoga Padilla <aoep.dev@gmail.com>
2024-10-28 03:13:57 -0400

    adjusted margin in contact, added favicon

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,7 +3,7 @@ import ContactForm from '../components/ContactForm';
 const Contact = () => (
   <section
     id="contact"
-    className="min-h-[calc(100vh-5.5rem)] flex flex-col justify-center items-center py-16 px-4 bg-gray-100 dark:bg-gray-900"
+    className="min-h-[calc(100vh-5.5rem)] flex flex-col justify-center items-center pt-28 px-4 bg-gray-100 dark:bg-gray-900"
   >
     <ContactForm />
   </section>
